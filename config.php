7d5533c8b3cf9874dcfb9a0d23a3a9a82163f218
Chamath Palihawadana <chamathpali123@gmail.com>
2017-06-09 16:17:30 +0530

    Initialize repo. with working SaveCompanies and Authentication

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	  // For more information http://developer.24sevenoffice.com/dev/#api-intro
+	  
+      $username = "USER";  //Change this to your client user or community login
+      $password = "PASS";  //Change this to your password
+      $applicationid = "APP_ID";  //Change this to your applicationId
+?>
