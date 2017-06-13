f0879fd94bad23c7a369a8888b2899725d5dbfe0
Chamath Palihawadana <chamathpali123@gmail.com>
2017-06-13 11:46:41 +0530

    Restructured and support for API POST request. SaveCompanies Method

--- index.php
+++ index.php
@@ -33,39 +33,66 @@
               $_SESSION['ASP.NET_SessionId'] = $result->LoginResult;
               // each seperate webservice need the cookie set
               $authentication->__setCookie("ASP.NET_SessionId", $_SESSION['ASP.NET_SessionId']);
-              print_r($authentication);
+              //print_r($authentication);
 
               // throw an error if the login is unsuccessful
               if($authentication->HasSession()->HasSessionResult == false)
                   throw new SoapFault("0", "Invalid credential information.");
           }
           // To get current identity:
-           print_r($authentication->GetIdentity());
+          $identity = json_encode($authentication->GetIdentity());
 
           // To connect to a 24seven webservice:
+           if($_GET['type']=='new'){
+              $data = json_decode(file_get_contents('php://input'),true );
+              $name = $data['Name'];
+              $email = $data['Email'];
+              $org_number = $data['Account_ID'];
+              
+              // Field Validations
+              if(empty($name) || empty($email) || empty($org_number)){
+                $res = array("status"=>0, "data"=> "Please send all the required fields", "identity"=>$identity);
+                echo json_encode($res);
+                die();
+              }
 
-          // START NEW COMPANY Params
-          $CompanyService = new SoapClient ( "https://api.24sevenoffice.com/CRM/Company/V001/CompanyService.asmx?WSDL", $options );
-          $CompanyService->__setCookie("ASP.NET_SessionId", $_SESSION['ASP.NET_SessionId']);
-          
-          $company["companies"][0]['Name'] ="FROM EARNSHARK";
-          $company["companies"][0]['Type'] ="Business";
-          $company["companies"][0]['OrganizationNumber'] ="EARNSHARK ID";
-          $company["companies"][0]['EmailAddresses']["Invoice"]["Value"] = "EMAIL";
-          $company["companies"][0]['EmailAddresses']["Invoice"]["Name"] = "EMAIL";
-          $company["companies"][0]['EmailAddresses']["Invoice"]["Description"] = "EMAIL";
+              $response_method = addCompany($name, $email, $org_number);
 
-          $company["companies"][0]['EmailAddresses']["Work"]["Value"] = "EMAIL";
-          $company["companies"][0]['EmailAddresses']["Work"]["Name"] = "EMAIL";
-          $company["companies"][0]['EmailAddresses']["Work"]["Description"] = "EMAIL";
+              if($response_method->SaveCompaniesResult->Company->APIException){
+                $res = array("status"=>0, "data"=> $response_method->SaveCompaniesResult->Company->APIException->Message, "identity"=>$identity);
+              }else{
+                $res = array("status"=>1, "data"=> $response_method->SaveCompaniesResult->Company, "identity"=>$identity);
+              }
+           }
 
-          $NewCompanyResult = ($CompanyService->SaveCompanies($company));
+          echo json_encode($res);
 
-          print_r($NewCompanyResult);
-          // END NEW COMPANY Params
       } 
       catch ( SoapFault $fault ) 
       {
           echo 'Exception: ' . $fault->getMessage();
       }
+
+      // Add a new Company to the CRM with the basic fields
+      function addCompany($name, $email, $org_number){
+          $options = array ('trace' => true );
+          $CompanyService = new SoapClient ( "https://api.24sevenoffice.com/CRM/Company/V001/CompanyService.asmx?WSDL", $options );
+          $CompanyService->__setCookie("ASP.NET_SessionId", $_SESSION['ASP.NET_SessionId']);
+          
+          $company["companies"][0]['Name'] = $name;
+          $company["companies"][0]['Type'] = "Business";
+          $company["companies"][0]['OrganizationNumber'] = $org_number;
+          $company["companies"][0]['EmailAddresses']["Invoice"]["Value"] = $email;
+          $company["companies"][0]['EmailAddresses']["Invoice"]["Name"] = $email;
+          $company["companies"][0]['EmailAddresses']["Invoice"]["Description"] = $email;
+
+          $company["companies"][0]['EmailAddresses']["Work"]["Value"] = $email;
+          $company["companies"][0]['EmailAddresses']["Work"]["Name"] = $email;
+          $company["companies"][0]['EmailAddresses']["Work"]["Description"] = $email;
+
+          $NewCompanyResult = ($CompanyService->SaveCompanies($company));
+
+          return $NewCompanyResult;
+      }
+
    ?>
